5c52947f66b6dee5fc225e592869f5404361eb65
kidari <48870192+kidari@users.noreply.github.com>
2020-10-20 15:53:05 +0800

    Update cookietest.py

--- cookietest.py
+++ cookietest.py
@@ -1,8 +1,8 @@
-import requests
+import requests,os
 jar = requests.cookies.RequestsCookieJar()
-jar.set('Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2', '1603174636', domain='.hifini.com', path='/')
-jar.set('bbs_token', 'HutOfjOUPTnUvskveYPKnHLH6_2BskyeH1M3q_2F_2BFZPQPzuSAEPxSLNDdu8oJ80_2FJ3I_2Frfo23KNB85kpplQb7BBHcdkUqA_3D', domain='.hifini.com', path='/')
-jar.set('bbs_sid', 'jq2vgg7dejacs5sihug10ni3d5', domain='.hifini.com', path='/')
+jar.set('Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2', os.environ["Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2"], domain='.hifini.com', path='/')
+jar.set('bbs_token', os.environ["bbs_token"], domain='.hifini.com', path='/')
+jar.set('bbs_sid', os.environ["bbs_sid"], domain='.hifini.com', path='/')
 url = 'https://www.hifini.com/sg_sign.htm'
 r = requests.post(url, cookies=jar)
-print(r.text)
+print('HIFI打卡完毕')
